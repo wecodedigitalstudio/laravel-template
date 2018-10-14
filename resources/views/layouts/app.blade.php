c4223bbf45e314f7d151c4f040114c82070f89e1
Giacomo Corallini <jackcoral89@4zur3.local>
2018-10-14 11:21:26 +0200

    asset css e js in app.blade per server

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,18 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Template</title>
-    <link rel="stylesheet" href="/css/custom.min.css">
-    <link rel="stylesheet" href="/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css">
-    <script src="/plugins/jquery/jquery.min.js"></script>
-    <script src="/plugins/bootstrap/bootstrap.min.js"></script>
-    <script src="/plugins/select2/select2.full.min.js"></script>
-    <script src="/plugins/select2/it.js"></script>
+    <link rel="stylesheet" href="{{ asset('/css/custom.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css') }}">
+    <script src="{{ asset('/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('/plugins/bootstrap/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/plugins/select2/select2.full.min.js') }}"></script>
+    <script src="{{ asset('/plugins/select2/it.js') }}"></script>
 </head>
 <body>
     
     @yield('content')
 
-    <script src="/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
-    <script src="/plugins/bootstrap-datepicker/js/bootstrap-datepicker.it.min.js"></script>
+    <script src="{{ asset('/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('/plugins/bootstrap-datepicker/js/bootstrap-datepicker.it.min.js') }}"></script>
 </body>
 </html>
